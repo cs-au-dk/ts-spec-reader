36836523e603f953165d7e7c1fee28e52bfaef14
Esben Andreasen <esbenandreasen@gmail.com>
2015-04-28 01:08:38 -0700

    command line: Environments...

--- src/CLI.ts
+++ src/CLI.ts
@@ -1,6 +1,6 @@
 /// <reference path="../typings/node/node.d.ts" />
 
-if(typeof Map === 'undefined' || typeof Set === 'undefined'){
+if (typeof Map === 'undefined' || typeof Set === 'undefined') {
     console.error("Map and Set missing. Run node with --harmony.");
     process.exit(1);
 }
@@ -16,13 +16,41 @@ var program = require('commander');
 program
     .version('0.1.0')
     .usage("[options]")
-    .description("Produces JSON for an environment described by TypeScript files. (hardcoded to es5 for now)")
+    .description("Produces JSON for an environment described by TypeScript files.")
+    .option("--env <env>", "Environment to read, one of: es5, es6, es5-dom, es6-dom", /^(es5|es6|es5-dom|es6-dom)$/)
     .option("-o --output <file>", "The file to output to")
     .parse(process.argv);
 
-var target = path.resolve(__dirname + "/../node_modules/typescript/bin/lib.core.d.ts");
+var es5 = path.resolve(__dirname + "/../node_modules/typescript/bin/lib.core.d.ts");
+var es6 = path.resolve(__dirname + "/../node_modules/typescript/bin/lib.core.es6.d.ts");
+var es5dom = path.resolve(__dirname + "/../node_modules/typescript/bin/lib.d.ts");
+var es6dom = path.resolve(__dirname + "/../node_modules/typescript/bin/lib.es6.d.ts");
 
-var result = Reader.readFiles([target]);
+var targets:string[];
+var env = program.env;
+if(env === true){
+    console.error("Missing --env");
+    process.exit(1);
+}
+switch (env) {
+    case 'es5-dom':
+        targets = [es5dom];
+        break;
+    case 'es6-dom':
+        targets = [es6dom];
+        break;
+    case 'es5':
+        targets = [es5];
+        break;
+    case 'es6':
+        targets = [es6];
+        break;
+    default:
+        console.error("Unhanleded env: %s", env);
+        process.exit(1);
+}
+
+var result = Reader.readFiles(targets);
 var json = JSON.stringify(result);
 if (program.output) {
     fs.writeFile(program.output, json, e => {
