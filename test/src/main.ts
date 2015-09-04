eca5cdb60f6941a9f690910d976c0d4e6059b1a4
Esben Andreasen <esbenandreasen@gmail.com>
2015-09-04 14:22:57 +0200

    improved tests

--- test/src/main.ts
+++ test/src/main.ts
@@ -9,7 +9,7 @@ import Reader = require('../../src/TypeScriptEnvironmentReader')
 
 it("Should work", function (done) {
     // Smoke test. Should not crash.
-    var result = Reader.readFiles(['test/fixtures/test1.ts', 'test/fixtures/test2.d.ts', 'node_modules/typescript/bin/typescript.d.ts', 'node_modules/typescript/bin/typescriptServices.d.ts']);
+    var result = Reader.readFiles(['typings/lib.d.ts', 'test/fixtures/test1.ts', 'test/fixtures/test2.d.ts', 'node_modules/typescript/bin/typescript.d.ts', 'node_modules/typescript/bin/typescriptServices.d.ts']);
     JSON.stringify(result);
     done();
 });
