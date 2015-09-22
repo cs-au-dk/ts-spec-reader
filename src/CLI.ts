92ef4584a5c177704319437a8e1ed7bb696727c7
Esben Andreasen <esbenandreasen@gmail.com>
2015-09-22 22:50:30 +0200

    add CLI support for plain node

--- src/CLI.ts
+++ src/CLI.ts
@@ -17,7 +17,7 @@ program
     .version('0.1.0')
     .usage("[options]")
     .description("Produces JSON for an environment described by TypeScript files.")
-    .option("--env <env>", "Environment to read, one of: es5, es6, es5-dom, es6-dom", /^(es5|es6|es5-dom|es6-dom)$/)
+    .option("--env <env>", "Environment to read, one of: es5, es6, es5-dom, es6-dom", /^(es5|es6|es5-dom|es6-dom|node)$/)
     .option("-o --output <file>", "The file to output to")
     .parse(process.argv);
 
@@ -25,11 +25,12 @@ var es5 = path.resolve(__dirname + "/../node_modules/typescript/bin/lib.core.d.t
 var es6 = path.resolve(__dirname + "/../node_modules/typescript/bin/lib.core.es6.d.ts");
 var es5dom = path.resolve(__dirname + "/../node_modules/typescript/bin/lib.d.ts");
 var es6dom = path.resolve(__dirname + "/../node_modules/typescript/bin/lib.es6.d.ts");
+var node = path.resolve(__dirname + "/../typings/node/node.d.ts");
 
 var targets:string[];
 var env = program.env;
-if(env === true){
-    console.error("Missing --env");
+if(!env){
+    console.error("Bad/Missing --env: %s", env);
     process.exit(1);
 }
 switch (env) {
@@ -44,6 +45,9 @@ switch (env) {
         break;
     case 'es6':
         targets = [es6];
+        break;
+    case 'node':
+        targets = [es6, node];
         break;
     default:
         console.error("Unhanleded env: %s", env);
