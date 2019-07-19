e264e0fb34b1bbddf55bedd002c09fb5a8b85c54
Anders Møller <amoeller@cs.au.dk>
2019-07-19 06:09:09 +0200

    build fixes

--- install.js
+++ install.js
@@ -1,3 +1,4 @@
 const execSync = require('child_process').execSync;
+const path = require('path');
 execSync("npm install --no-audit --loglevel=error");
-execSync("tsc", {cwd: "node_modules/.bin"});
+execSync("." + path.sep + "tsc", {cwd: "node_modules/.bin"});
